3c35bd67d198cd8d687addd60bb2374de7ab0797
khushbooG9 <kgn9294@gmail.com>
2020-11-11 11:37:28 -0800

    Added some visual code

--- index.py
+++ index.py
@@ -30,6 +30,66 @@ def build_banner():
         ],
     )
 
+def build_settings_tab():
+    """
+    Function to put together the settings tab
+    """
+    return [
+    #system_configuration_panel(),
+        html.Div(
+            id="system-configuration-menu",
+            children = html.H2("No settings yet"),
+        )]
+
+def build_buttons_panel():
+    """
+    Function to generate a panel for buttons on left side
+    """
+    return html.Div(
+      id="buttons-panel",
+      className="row",
+      children=[
+          html.Div(
+              id="card-1",
+              children=[
+                  html.Button( className="", id="button1",children="Arbitrage",n_clicks=0),
+                  html.Button( className="", id="button1", children="Peak Shaving", n_clicks=0),
+                  html.Button( className="", id="button1", children="Demand Shaping", n_clicks=0),
+                  html.Button( className="", id="button1", children="System Capacity", n_clicks=0),
+                  html.Button( className="", id="button1", children="Frequency Regulation", n_clicks=0),
+                  html.Button( className="", id="button1", children="Spinning/Non-Spinning Reserve", n_clicks=0),
+                  html.Button( className="", id="button1", children="Volt/Var", n_clicks=0),
+                  html.Button( className="", id="button1", children="Co-optimize", n_clicks=0),
+              ],
+          ),
+          html.Div(
+              id="card-2",
+              children=[
+                  html.Button("Data Input", className="", id="button2"),
+                 
+              ],
+          ),
+          
+      ],
+
+    )
+
+def build_simulation_tab():
+    """
+    Function to put together the simulation  tab
+    """
+    return  ( html.Div(
+        id="simulation-container",
+        children = [
+            build_buttons_panel(),
+            html.Div(
+                id="graphs-container",
+                children = html.H2("Working on it"),
+            ),
+        ],
+    ),
+    )
+
 def build_tabs():
     """
     Function to build both the tabs
@@ -52,7 +112,7 @@ def build_tabs():
                     ),
                     dcc.Tab(
                         id="Control-chart-tab",
-                        label="Simulation Dashboard",
+                        label="Control Dashboard",
                         value="tab2",
                         className="custom-tab",
                         selected_className="custom-tab--selected",
@@ -73,7 +133,8 @@ def serve_layout():
             id="app-container",
             children=[
                 build_tabs(),
-                html.H6("Starter")
+                html.Div(id="app-content"),
+                
             ],
         ),
 
@@ -81,6 +142,20 @@ def serve_layout():
   )
 
 app.layout = serve_layout
+
+@app.callback(
+    output = [Output("app-content", "children")],
+    inputs =        [Input("app-tabs","value")],
+    #state = [State("value-setter-store","data"), State("dropdown-store", "data")]
+    )
+def render_tab_contents(tab_switch):
+    """
+    """
+    if tab_switch=="tab2":
+        
+        return build_simulation_tab()
+
+    return build_settings_tab()
 
 if __name__ == '__main__':
     app.run_server(debug=True)
