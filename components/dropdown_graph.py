73f155bddf33fdf764a3fdcb41baa04db582be80
Kini, Roshan L <roshan.kini@pnnl.gov>
2021-08-02 10:54:31 -0700

    removed unused functions and created a commom_slider function

--- components/dropdown_graph.py
+++ components/dropdown_graph.py
@@ -3,7 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 
 
-def common_dropdown_box(id: str, default_value:str):
+def common_dropdown_box(id: str, default_value: str):
     """
     id: the html element that is created such that you can style/use javascript to manipulate
         (MUST BE UNIQUE ACROSS ALL ELEMENTS ON THE PAGE)
@@ -13,19 +13,20 @@ def common_dropdown_box(id: str, default_value:str):
     if default_value not in options:
         raise ValueError(f'Invalid option specified must be one of ({",".join(options)})')
     return dcc.Dropdown(
-            id=id, # 'fig-left-dropdown',
-            options=[
-                {'label': 'Grid Import', 'value': 'GI'},
-                {'label': 'Peak Load', 'value': 'PL'},
-                {'label': 'Grid Reactive', 'value': 'GR'},
-                {'label': 'Battery Reactive', 'value': 'BR'},
-                {'label': 'Power Factor', 'value': 'PF'},
-                {'label': 'Energy Price', 'value': 'EP'},
-                {'label': 'Demand', 'value': 'D'}
-            ],
-            #placeholder="Left y-axis (default = Grid Import)",
-            value=default_value # 'GI'
-        )
+        id=id,  # 'fig-left-dropdown',
+        options=[
+            {'label': 'Grid Import', 'value': 'GI'},
+            {'label': 'Peak Load', 'value': 'PL'},
+            {'label': 'Grid Reactive', 'value': 'GR'},
+            {'label': 'Battery Reactive', 'value': 'BR'},
+            {'label': 'Power Factor', 'value': 'PF'},
+            {'label': 'Energy Price', 'value': 'EP'},
+            {'label': 'Demand', 'value': 'D'}
+        ],
+        # placeholder="Left y-axis (default = Grid Import)",
+        value=default_value  # 'GI'
+    )
+
 
 def common_graph(id: str, dropdown_default: Optional[str] = None):
     """
@@ -47,8 +48,32 @@ def common_graph(id: str, dropdown_default: Optional[str] = None):
         children.insert(0, common_dropdown_box(id=f"{id}-dropdown", default_value=dropdown_default))
     return html.Div(children)
 
-def common_slider(id:str, default_value):
-    """
 
+def common_slider(id: str, className: str, label_name, label_style,
+                  default_value=0, min=-100, max=100, step=20, updatemode='drag'):
     """
-    pass
+    Create a slide component
+    """
+    marks = {-100: '-100 %',
+             -75: '-75 %',
+             -50: '-50 %',
+             -25: '-25 %',
+             0: '0 %',
+             25: '25 %',
+             50: '50 %',
+             75: '75 %',
+             100: '100 %'
+             }
+    return html.Div([
+        html.Label(label_name, style=label_style),
+        dcc.Slider(
+            id=id,
+            className=className,
+            min=min,
+            max=max,
+            step=step,
+            marks=marks,
+            value=default_value,
+            updatemode=updatemode
+        ),
+    ])
--- simulation_layout.py
+++ simulation_layout.py
@@ -2,12 +2,12 @@ import dash_html_components as html
 import dash_core_components as dcc
 import dash_daq as daq
 import plotly.graph_objects as go
-from components.dropdown_graph import common_graph
+from components.dropdown_graph import common_graph, common_slider
 
 style = {'width': '100%', 'height': '30px', 'lineHeight': '30px', 'borderWidth': '1px', 'borderStyle': 'dashed',
          'borderRadius': '2px', 'textAlign': 'center', 'margin': '10px', 'fontSize': '12px'}
-label_style = {'textAlign': 'center',  'fontSize': '16px'}
-label_style_1 = {'textAlign': 'left',  'fontSize': '15px'}
+label_style = {'textAlign': 'center', 'fontSize': '16px'}
+label_style_1 = {'textAlign': 'left', 'fontSize': '15px'}
 
 
 def build_simulation_tab():
@@ -26,68 +26,6 @@ def build_simulation_tab():
             ),
         ]
     )
-
-
-
-def build_price_change_button():
-    return html.Div(
-        id="card-1",
-        children=[html.Button("Price change in load", className="", id="button1", n_clicks=0),
-                  # html.Div(id='price-change-button'),
-                  ],
-    )
-
-
-def build_price_change_slider():
-    return html.Div([
-        html.Label('Price Change', style=label_style),
-        dcc.Slider(
-            id='price-change-slider',
-            className='price-change-slider',
-            min=-100,
-            max=100,
-            step=20,
-            marks={
-                -100: '-100 %',
-                -75: '-75 %',
-                -50: '-50 %',
-                -25: '-25 %',
-                0: '0 %',
-                25: '25 %',
-                50: '50 %',
-                75: '75 %',
-                100: '100 %'
-            },
-            value=0,
-            updatemode='drag'
-        ),
-    ])
-
-
-def grid_load_change_slider():
-    return html.Div([
-        html.Label('Load Change', style=label_style),
-        dcc.Slider(
-            id='grid-load-change-slider',
-            className='grid-load-change-slider',
-            min=-100,
-            max=100,
-            step=20,
-            marks={
-                -100: '-100 %',
-                -75: '-75 %',
-                -50: '-50 %',
-                -25: '-25 %',
-                0: '0 %',
-                25: '25 %',
-                50: '50 %',
-                75: '75 %',
-                100: '100 %'
-            },
-            value=0,
-            updatemode='drag'
-        ),
-    ])
 
 
 def build_unscheduled_outage_button():
@@ -138,42 +76,43 @@ def build_simulation_input_controls():
             html.Div(
                 id="data_resolution",
                 children=[
-                    dcc.Input(id='update-data-resolution-box', type="number", min=1, max=3600, step=60, value=1, className="input-number-box"),
+                    dcc.Input(id='update-data-resolution-box', type="number", min=1, max=3600, step=60, value=1,
+                              className="input-number-box"),
                     html.Button('Data Resolution', id='submit-data-resolution', n_clicks=0,
                                 className="button")
-            ]),
+                ]),
             html.Div(
-                id="start-timer", 
-                children=[ 
+                id="start-timer",
+                children=[
                     dcc.Input(id='start-time', type="number", min=0, max=5000, step=1, value=0),
                     html.Button('Start Time', id='submit-start-time', n_clicks=0, className="button")
-            ]),
-            html.Div(id="stop-timer", 
-                children=[
-                    dcc.Input(id='stop-time', type="number", min=0, max=3600*24, step=1, value=0),
-                    html.Button('Stop Time', id='submit-stop-time', n_clicks=0, className="button")
-            ]),
-            html.Div(id="rate", 
-                children=[
-                    dcc.Input(id='update-rate-box',type="number", min=800, max=5000, step=100, value=1000),
-                    html.Button('Update Rate', id='submit-rate', n_clicks=0, className="button")
-            ]),
-            html.Div(id="window", 
-                children=[
-                    dcc.Input(id='update-window', type="number", min=20, max=3600*24, step=10, value=120),
-                    html.Button('Update Window', id='submit-update-window', n_clicks=0,
-                                className="button")
-            ]),
-            html.Div(id="controller-update-rate", 
-                children=[
-                    dcc.Input(id='update-controller-rate-box', type="number", min=60, max=3600, step=60, value=60),
-                    html.Button('Controller Update Rate', id='submit-controler-rate-update', n_clicks=0,
-                                className="button")
-            ]),
+                ]),
+            html.Div(id="stop-timer",
+                     children=[
+                         dcc.Input(id='stop-time', type="number", min=0, max=3600 * 24, step=1, value=0),
+                         html.Button('Stop Time', id='submit-stop-time', n_clicks=0, className="button")
+                     ]),
+            html.Div(id="rate",
+                     children=[
+                         dcc.Input(id='update-rate-box', type="number", min=800, max=5000, step=100, value=1000),
+                         html.Button('Update Rate', id='submit-rate', n_clicks=0, className="button")
+                     ]),
+            html.Div(id="window",
+                     children=[
+                         dcc.Input(id='update-window', type="number", min=20, max=3600 * 24, step=10, value=120),
+                         html.Button('Update Window', id='submit-update-window', n_clicks=0,
+                                     className="button")
+                     ]),
+            html.Div(id="controller-update-rate",
+                     children=[
+                         dcc.Input(id='update-controller-rate-box', type="number", min=60, max=3600, step=60, value=60),
+                         html.Button('Controller Update Rate', id='submit-controler-rate-update', n_clicks=0,
+                                     className="button")
+                     ]),
             # This will be for handling clicks on any of the buttons so they route properly to update the 
             # graphs
             html.Div(id='clicked-button', children='del:0 add:0 tog:0 last:nan', style={'display': 'none'})
-    ])
+        ])
 
 
 def build_simulation_controls():
@@ -186,16 +125,15 @@ def build_simulation_controls():
         children=[
             build_simulation_input_controls(),
             html.Br(),
-            build_price_change_slider(),
-            grid_load_change_slider(),
+            common_slider(id="price-change-slider", className="price-change-slider", label_name="Price Change",
+                          label_style=label_style),
+            common_slider(id='grid-load-change-slider', className='grid-load-change-slider', label_name="Load Change",
+                          label_style=label_style),
             html.Br(),
             build_unscheduled_outage_button(),
             html.Br(),
             build_external_signal_button(),
             html.Br(),
-            #html.Br(),
-            #ess_parameter_block_1(),
-            #ess_parameter_block_2(),
             revenue_block(),
             build_stop_button(),
         ],
@@ -205,37 +143,37 @@ def build_simulation_controls():
 def revenue_block():
     return html.Div(
         [html.Label("Revenue", style=label_style),
-        html.Div(
-        id="revenue-block",
-        className="row",
-        children=[
-            html.Div(
-                id="revenue-label1",
-                children=[
-                    html.Label("Day Ahead Estimate", style=label_style_1),
-                    dcc.Input(id="revenue1", type='text', disabled=True, style={"width": '70%'}),
-                ]
-            ),
+         html.Div(
+             id="revenue-block",
+             className="row",
+             children=[
+                 html.Div(
+                     id="revenue-label1",
+                     children=[
+                         html.Label("Day Ahead Estimate", style=label_style_1),
+                         dcc.Input(id="revenue1", type='text', disabled=True, style={"width": '70%'}),
+                     ]
+                 ),
 
-            #html.Br(),
-            html.Div(
-                id="revenue-label2",
-                children=[
-                    html.Label("Actual, Not Adjusted", style=label_style_1),
-                    dcc.Input(id="revenue2", type='text', disabled=True, style={"width": '70%'}),
-                ]
-            ),
+                 # html.Br(),
+                 html.Div(
+                     id="revenue-label2",
+                     children=[
+                         html.Label("Actual, Not Adjusted", style=label_style_1),
+                         dcc.Input(id="revenue2", type='text', disabled=True, style={"width": '70%'}),
+                     ]
+                 ),
 
-            #html.Br(),
-            html.Div(
-                id="revenue-label3",
-                children=[
-                    html.Label("Actual, Real Time Adjusted", style=label_style_1),
-                    dcc.Input(id="revenue3", type='text', disabled=True, style={"width": '70%'}),
-                ]
-            ),
+                 # html.Br(),
+                 html.Div(
+                     id="revenue-label3",
+                     children=[
+                         html.Label("Actual, Real Time Adjusted", style=label_style_1),
+                         dcc.Input(id="revenue3", type='text', disabled=True, style={"width": '70%'}),
+                     ]
+                 ),
 
-        ])])
+             ])])
 
 
 def build_top_panel():
